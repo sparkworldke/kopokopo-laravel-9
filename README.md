910b8f72fff2b9be3d9c8bc0e6c7fd0fe9139fbb
SparkWorld Enterprises <sparkworldke@gmail.com>
2024-03-29 03:36:11 +0300

    Project Setup

---
+++ README.md
@@ -0,0 +1,7 @@
+# Project Breakdown
+
+1. Frontend: Home, About, Categories, Single Category, Resources, Single Resource, Purchase Resource,  Success page with download link, Send an email with resource
+2. Backend/Admin/User (Guest Uploader): Login, Register, Details, Privileges, Admin/User, Manage Categories, Resources, Settings, About,
+3. Use Chat GPT to create the template and code the website
+4. Add necessary plugins: Breeze for registration, Spatie Multiauth, SEO by Rezipp, Notify, Livewire, Mobile detect, share with social media, Pdf to image, doc to image
+5. Extras: Save keywords from search, Email Marketing plugin, Push notifications, Social proof plugin 
---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'email_verified_at' => now(),
+            'phone_number' => 254718020630,
+            'mpesa_phone_number' => 254718020630,
+            'whatsapp_phone_number' => 254718020630,
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        ])->assignRole('author', 'admin');
+        User::create([
+            'name' => 'author',
+            'email' => 'autho@gmail.com',
+            'email_verified_at' => now(),
+            'phone_number' => 254718020631,
+            'mpesa_phone_number' => 254718020631,
+            'whatsapp_phone_number' => 254718020631,
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        ])->assignRole('author');
+    }
+}
